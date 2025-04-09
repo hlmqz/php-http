6c9b592b829a67a99241bbb35be234ae7f640f31
H Marquez <hlmarquezm@gmail.com>
2025-04-08 22:54:07 -0500

    feat:: se agrega urlinfo y se cambia nombre a dataSended para mas descriptivo

--- src/Responded.php
+++ src/Responded.php
@@ -8,16 +8,17 @@ class Responded
 	{
 		get => $this->curlInfo['total_time_us']/1000000 ?? 0;
 	}
-	
+
 	function __construct(
 		private(set) int $httpCode = 0,
 		private(set) string $contentType = '',
+		private(set) array $urlInfo = [],
 		private(set) string $url = '',
 		private(set) string $method = '',
 		private(set) array $headers = [],
 		private(set) object|array|string|null $content = null,
 		private(set) mixed $error = null,
-		private(set) object|array $rawData = [],
+		private(set) object|array $dataSended = [],
 		private(set) object|array $curlInfo = [],
 	)
 	{}
